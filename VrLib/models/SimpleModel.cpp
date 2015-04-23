bbdfb43f5381fbfbdf92895f70d3ae6056c91a24
Johan Talboom <borfje@gmail.com>
2015-04-23 16:45:35 +0200

    Added sphere model

--- VrLib/models/SimpleModel.cpp
+++ VrLib/models/SimpleModel.cpp
@@ -2,6 +2,10 @@
 #include <VrLib/Log.h>
 #include <VrLib/util.h>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
 namespace vrlib
 {
 	template<class VertexFormat>
@@ -50,6 +54,54 @@ namespace vrlib
 				indices.push_back(i + 0);
 			}
 		}
+		else if (parameters[0] == "sphere")
+		{
+			int stacks = 8;
+			int slices = 8;
+
+			float thetaInc = (float)M_PI / stacks;
+			float phiInc = (float)(2 * M_PI / slices);
+
+			for (float theta = -(float)M_PI_2; theta < (float)M_PI_2; theta += thetaInc)
+			{
+				for (float phi = 0; phi < (float)(2 * M_PI); phi += phiInc)
+				{
+					float x1 = glm::cos(theta) * glm::cos(phi);
+					float y1 = glm::cos(theta) * glm::sin(phi);
+					float z1 = glm::sin(theta);
+
+					float x2 = glm::cos(theta + thetaInc) * glm::cos(phi + phiInc);
+					float y2 = glm::cos(theta + thetaInc) * glm::sin(phi + phiInc);
+					float z2 = glm::sin(theta + thetaInc);
+
+					float x3 = glm::cos(theta + thetaInc) * glm::cos(phi);
+					float y3 = glm::cos(theta + thetaInc) * glm::sin(phi);
+					float z3 = glm::sin(theta + thetaInc);
+
+					float x4 = glm::cos(theta) * glm::cos(phi + phiInc);
+					float y4 = glm::cos(theta) * glm::sin(phi + phiInc);
+					float z4 = glm::sin(theta);
+
+					VertexFormat v;
+					setP3(v, glm::vec3(x1, y1, z1));		setN3(v, glm::vec3(x1, y1, z1));		setT2(v, glm::vec2(asin(x1)/M_PI + 0.5f, asin(y1)/M_PI + 0.5f));		verts.push_back(v);
+					setP3(v, glm::vec3(x2, y2, z2));		setN3(v, glm::vec3(x2, y2, z2));		setT2(v, glm::vec2(asin(x2)/M_PI + 0.5f, asin(y2)/M_PI + 0.5f));		verts.push_back(v);
+					setP3(v, glm::vec3(x3, y3, z3));		setN3(v, glm::vec3(x3, y3, z3));		setT2(v, glm::vec2(asin(x3)/M_PI + 0.5f, asin(y3)/M_PI + 0.5f));		verts.push_back(v);
+					setP3(v, glm::vec3(x4, y4, z4));		setN3(v, glm::vec3(x4, y4, z4));		setT2(v, glm::vec2(asin(x4)/M_PI + 0.5f, asin(y4)/M_PI + 0.5f));		verts.push_back(v);
+				}
+			}
+			indices.clear();
+			for (size_t i = 0; i < verts.size(); i += 4)
+			{
+				indices.push_back(i + 0);
+				indices.push_back(i + 1);
+				indices.push_back(i + 2);
+				
+				indices.push_back(i + 3);
+				indices.push_back(i + 1);
+				indices.push_back(i + 0);
+			}
+
+		}
 		else
 			logger << "Unknown simplemodel: " << fileName << Log::newline;
 
@@ -57,6 +109,10 @@ namespace vrlib
 
 		if (!verts.empty())
 		{
+			for (size_t i = 0; i < verts.size(); i++)
+				vertices.push_back(glm::vec3(verts[i].px, verts[i].py, verts[i].pz));
+
+
 			vao = new gl::VAO<VertexFormat>(&vbo);
 
 			vbo.bind();
@@ -76,7 +132,7 @@ namespace vrlib
 	template<class VertexFormat>
 	std::vector<glm::vec3> SimpleModel<VertexFormat>::getVertices(int amount)
 	{
-		return std::vector<glm::vec3>();
+		return vertices;
 	}
 
 	template<class VertexFormat>
