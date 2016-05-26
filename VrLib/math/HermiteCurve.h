1cbb8ac26c3ab5ac06ffab340d0c46bdb0c5b3ae
Johan Talboom <borfje@gmail.com>
2016-05-26 13:00:42 +0200

    Added hermite curve

---
+++ VrLib/math/HermiteCurve.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+
+namespace vrlib
+{
+	namespace math
+	{
+
+		class HermiteCurve
+		{
+		public:
+			glm::vec2 p0;
+			glm::vec2 p1;
+
+			glm::vec2 m0;
+			glm::vec2 m1;
+
+
+			HermiteCurve(glm::vec2 pos0, glm::vec2 dir0, glm::vec2 pos1, glm::vec2 dir1);
+
+			glm::vec2 getPoint(float t) const;
+
+		};
+	}
+}
