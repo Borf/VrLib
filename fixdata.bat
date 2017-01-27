ace118d07e5c0436d5905e194e07bfade91d6fb0
Johan Talboom <borfje@gmail.com>
2017-01-27 11:32:36 +0100

    Fixed a quote issue

--- fixdata.bat
+++ fixdata.bat
@@ -6,16 +6,16 @@ cd %~DP0
 IF NOT EXIST ..\..\..\Runtime GOTO byebye
 IF NOT EXIST "%~2data" GOTO byebye
 echo Detected we're in VrEnvironment
-xcopy /q /y "%1" ..\..\..\Runtime
+xcopy /q /y %1 ..\..\..\Runtime
 robocopy /nc /ns /np /njh /njs /nfl /ndl /e "%~2data" "..\..\..\Runtime\data"
 if NOT EXIST w: GOTO byebye
 if NOT EXIST x: GOTO byebye
 if NOT EXIST y: GOTO byebye
 if NOT EXIST z: GOTO byebye
-xcopy /q /y "%1" w:\Runtime
-xcopy /q /y "%1" x:\Runtime
-xcopy /q /y "%1" y:\Runtime
-xcopy /q /y "%1" z:\Runtime
+xcopy /q /y %1 w:\Runtime
+xcopy /q /y %1 x:\Runtime
+xcopy /q /y %1 y:\Runtime
+xcopy /q /y %1 z:\Runtime
 
 
 :byebye
